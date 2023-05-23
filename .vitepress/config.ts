421d52738313a5d1eaa1bbd3769d1e738259bfa0
小莫唐尼 <1431128779@qq.com>
2023-05-23 11:25:13 +0800

    修改：自定义404页面内容

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -68,15 +68,52 @@ export default defineConfig({
         link: 'https://b.925i.cn'
       }
     ],
-    localeLinks: {
-      text: '简体中文',
-      items: []
+    search: {
+      provider: 'local',
+      options: {
+        translations: {
+          button: {
+            buttonText: '搜索文档',
+            buttonAriaLabel: '搜索文档'
+          },
+          modal: {
+            noResultsText: '无法找到相关结果',
+            resetButtonTitle: '清除查询条件',
+            displayDetails: '禁用详细视图',
+            backButtonTitle: '返回',
+            footer: {
+              selectText: '选择',
+              navigateText: '切换',
+              closeText: '关闭'
+            }
+          }
+        }
+        // locales: {
+        // 	zh: {
+        // 		translations: {
+        // 			button: {
+        // 				buttonText: "搜索文档",
+        // 				buttonAriaLabel: "搜索文档",
+        // 			},
+        // 			modal: {
+        // 				noResultsText: "无法找到相关结果",
+        // 				resetButtonTitle: "清除查询条件",
+        // 				footer: {
+        // 					selectText: "选择",
+        // 					navigateText: "切换",
+        // 				},
+        // 			},
+        // 		},
+        // 	},
+        // },
+      }
     },
-    algolia: {
-      appId: 'Y93HSGWS5S',
-      apiKey: '55af29e9571ea6fe09352c04fdc309e9',
-      indexName: 'ialley-workshop'
-    },
+
+    // algolia: {
+    //   appId: 'Y93HSGWS5S',
+    //   apiKey: '55af29e9571ea6fe09352c04fdc309e9',
+    //   indexName: 'ialley-workshop'
+    // },
     footer: {
       message: '根据 AGPL-3.0 协议开源·请遵循协议',
       copyright:
--- .vitepress/theme/components/CustomLayout.vue
+++ .vitepress/theme/components/CustomLayout.vue
@@ -16,6 +16,15 @@ const data = useData();
                 <img class="ad-image" src="https://img.925i.cn/file/d5971b7a6f68900ff2d5a.png" alt="广告位招租" data-fancybox="gallery">
             </div>
         </template>
+        <template #not-found>
+            <div class="NotFound">
+                <p class="code">404</p>
+                <h1 class="title">啊偶，页面找不到了</h1>
+                <div class="divider"></div>
+                <blockquote class="quote">您访问的地址不存在或者已经被删除，如果您有需要，请发送邮件到 <a href="mailto:studio@925i.cn">studio@925i.cn</a>获取支持. </blockquote>
+                <div class="action"><a class="link" href="/" aria-label="返回主页"> 返回主页 </a></div>
+            </div>
+        </template>
     </Layout>
 </template>
 <style scoped lang="less">
@@ -58,5 +67,53 @@ const data = useData();
 
 .ad-image {
     margin-top: 12px;
+}
+
+.NotFound {
+    width: 100%;
+    height: 500px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    .code {
+        line-height: 64px;
+        font-size: 64px;
+        font-weight: 600;
+    }
+    .title {
+        padding-top: 12px;
+        letter-spacing: 2px;
+        line-height: 20px;
+        font-size: 20px;
+        font-weight: 700;
+    }
+    .divider {
+        margin: 24px auto 18px;
+        width: 64px;
+        height: 1px;
+        background-color: var(--vp-c-divider);
+    }
+    .quote {
+        margin: 0 auto;
+        max-width: 256px;
+        font-size: 14px;
+        font-weight: 500;
+        color: var(--vp-c-text-2);
+    }
+    .action {
+        padding-top: 20px;
+        .link {
+            display: inline-block;
+            border: 1px solid var(--vp-c-brand);
+            border-radius: 16px;
+            padding: 3px 16px;
+            font-size: 14px;
+            font-weight: 500;
+            color: var(--vp-c-brand);
+            transition: border-color 0.25s, color 0.25s;
+        }
+    }
 }
 </style>
