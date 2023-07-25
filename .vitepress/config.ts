686d3685a7c3151515906436ee7cb89d4dc5534f
小莫唐尼 <1431128779@qq.com>
2023-07-25 10:00:12 +0800

    fix：修复图片预览失效问题

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -28,13 +28,13 @@ export default defineConfig({
       'link',
       {
         rel: 'stylesheet',
-        href: 'https://cdn.jsdelivr.net/npm/@fancyapps/ui/dist/fancybox.css'
+        href: 'https://fastly.jsdelivr.net/npm/@fancyapps/ui/dist/fancybox.css'
       }
     ],
     [
       'script',
       {
-        src: 'https://cdn.jsdelivr.net/npm/@fancyapps/ui@4.0/dist/fancybox.umd.js'
+        src: 'https://fastly.jsdelivr.net/npm/@fancyapps/ui@4.0/dist/fancybox.umd.js'
       }
     ],
     [
