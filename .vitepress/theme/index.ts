4e074a6bfcd2ade32aaedcc0522f6f77dc9b6932
小莫唐尼 <tzxiaomo@outlook.com>
2024-11-29 19:31:15 +0800

    feat: 页面添加评论

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -28,6 +28,8 @@ import {
 } from '@nolebase/vitepress-plugin-enhanced-readabilities/client';
 import '@nolebase/vitepress-plugin-enhanced-readabilities/client/style.css';
 
+import giscusTalk from 'vitepress-plugin-comment-with-giscus';
+import { useData, useRoute } from 'vitepress';
 
 const theme: ThemeConfig = {
 	extends: DefaultTheme,
@@ -59,6 +61,30 @@ const theme: ThemeConfig = {
 			}
 			// 其他配置...
 		});
-	}
+	},
+	setup() {
+		// Get frontmatter and route
+		const { frontmatter } = useData();
+		const route = useRoute();
+
+		// giscus配置
+		giscusTalk({
+				repo: 'ialley-workshop-open/uni-halo-doc', //仓库
+				repoId: 'R_kgDOMTa01A', //仓库ID
+				category: 'Announcements', // 讨论分类
+				categoryId: 'DIC_kwDOMTa01M4Ckt3n', //讨论分类ID
+				mapping: 'pathname',
+				inputPosition: 'bottom',
+				lang: 'zh-CN',
+			},
+			{
+				frontmatter, route
+			},
+			//默认值为true，表示已启用，此参数可以忽略；
+			//如果为false，则表示未启用
+			//您可以使用“comment:true”序言在页面上单独启用它
+			true
+		);
+	},
 };
 export default theme;
